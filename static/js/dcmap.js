803647b0b9322cc4c3db0ffb890f8eb241fe149f
james <james@jnt-mobile.jnt>
2015-07-17 01:57:39 -0500

    Now shows countries with right color ranges based on

--- static/js/dcmap.js
+++ static/js/dcmap.js
@@ -14,15 +14,22 @@ function makeGraphs(error, sitesJson, worldJson) {
 	var ndx = crossfilter(topSites);
 
 	// dimensions
-        var countryDim = ndx.dimension(function(d) { return d["school_state"]; });
+        var countryDim = ndx.dimension(function(d) { 
+		if (typeof d["ips"][0] !== 'undefined' ) {
+			if (typeof d["ips"][0]["country"] !== 'undefined' ) {
+				return d["ips"][0]["country"];
+			}
+		}});
 
 	// metrics
 	var totalIpsByCountry = countryDim.group().reduceCount(function(d) {
-		if (typeof d["ips"][0]["country"] !== 'undefined' ) {
-			return d["ips"][0]["country"];
-		}
-        });
+                if (typeof d["ips"][0] !== 'undefined' ) {
+                        if (typeof d["ips"][0]["country"] !== 'undefined' ) {
+                                return d["ips"][0]["country"];
+                        }
+                }});
 
+	
 	// var max_country = totalIpsByCountry.top(1)[0].value;
 
 	//var worldDim = ndx.dimension(function(d) { return d['
@@ -32,10 +39,11 @@ function makeGraphs(error, sitesJson, worldJson) {
 		.group(totalIpsByCountry)
 		.colors(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])
 		//.colorDomain([0, max_country])
-		.colorDomain([0, 5])
+		.colorDomain([0, 10])
 		.overlayGeoJson(worldJson["features"], "country", function (d) {
+			//if (d.properties.featurecla == "Admin-0 country") {
 			if (d.properties.featurecla == "Admin-0 country") {
-				return d.properties.sov_a3;
+				return d.properties.iso_a3;
 			}
 		})
 		.projection(d3.geo.conicEquidistant()
